bd94de07ba60e9cde29334fea5a6dccbade4a6b0
muyaszed <muyaszed@gmail.com>
2016-09-24 11:24:31 +0100

    import ReadingTime imto reactReading time

--- example/react-reading-time.jsx
+++ example/react-reading-time.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// var React = require('react');
-// var ReactDOM = require('react-dom');
+import ReadingTime from '../src/reading-time'
 
 class ReactReadingTime extends React.Component {
+	constructor(props) {
+	  super(props);
+
+	  this.state = {
+	    text: 'Foo is baz and bar'
+	  };
+	}
+
 	render() {
-		return (
-			React.createElement('div', {className: 'container'}, 'Hello My React!')
-		) 
+	  return (
+	    <div className='container' style={{ marginTop: '50px' }}>
+	      <div className='col-lg-8 col-lg-offset-2 form-group'>
+	        <textarea
+	          value={this.state.text}
+	          className='form-control'
+	          style={{ height: '500px', resize: 'none' }}>
+	        </textarea>
+	      </div>
+	      <ReadingTime text={this.state.text} className='col-lg-2 well' />
+	    </div>
+	  );
 	}
 }
 
